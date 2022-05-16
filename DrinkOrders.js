952cbc335c4c29b2fab32717f30fa9e2070d1378
Breanna Bang <101009515+BreaBang@users.noreply.github.com>
2022-05-15 20:24:05 -0600

    Add files via upload

---
+++ DrinkOrders.js
@@ -0,0 +1,12 @@
+//function that returns a drink based on age
+function peopleWithAgeDrink(old) {
+  if (old <= 13){
+    return 'drink toddy'
+  } else if (old <= 17) {
+    return 'drink coke'
+    } else if (old <= 20) {
+      return 'drink beer'
+    } else {
+      return 'drink whisky'
+    }
+  }
