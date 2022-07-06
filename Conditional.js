7dadc09b06aec8d031c6c169e6dc6f875a27d039
Breanna Bang <101009515+BreaBang@users.noreply.github.com>
2022-07-06 06:17:56 -0600

    Add files via upload

---
+++ Conditional.js
@@ -0,0 +1,13 @@
+//Inspired by the development team at Vooza, write the function that
+
+//accepts the name of a programmer, and
+//returns the number of lightsabers owned by that person.
+//note only zach has lightsabers and he has 18
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === "Zach"){
+  return 18
+    } else {
+      return 0
+    }
+}
